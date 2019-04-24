6180e071bfa05b921a9f3efc70e46be5b573dd50
JC Romualdo <juan.romualdo@zoro.com>
2019-04-24 10:09:56 -0500

    server

--- server.js
+++ server.js
@@ -3,34 +3,36 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const morgan = require('morgan');
 
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
+// Database Setup
 const db = knex({
   client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'aneagoie',
-    password : '',
-    database : 'smart-brain'
-  }
+  connection: process.env.POSTGRES_URI
 });
 
 const app = express();
 
+app.use(morgan('combined'))
 app.use(cors())
-app.use(bodyParser.json());
+//app.use(bodyParser.json())
+app.use(bodyParser.json({
+  extended: true
+}));
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send(" NO WORRIES, IT WORKS! ") })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.post('./profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db)}) //profile update
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+  console.log('Welcome JC, app is running on port 3000');
 })
